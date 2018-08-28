05b679d9c55aa1cbae137057b6558d4dbcc8fd17
lndsilva <lndsilva@hotmail.com>
2018-08-28 17:21:45 -0300

    Initial commit

---
+++ app/src/main/java/br/senac/sp/l13/pagueaquiapp/ConsultaRecarga.java
@@ -0,0 +1,13 @@
+package br.senac.sp.l13.pagueaquiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConsultaRecarga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.consulta_recarga_layout);
+    }
+}
---
+++ app/src/main/java/br/senac/sp/l13/pagueaquiapp/RecargaActivity.java
@@ -0,0 +1,20 @@
+package br.senac.sp.l13.pagueaquiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class RecargaActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recarga_layout);
+
+
+
+
+
+    }
+}
